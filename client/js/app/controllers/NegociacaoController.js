17657a73edcd7e74d4538d38d15e5f1d11290d95
karin-jpg <andradkarin@gmail.com>
2021-03-10 20:59:13 -0300

    Criação da classe Datehelper e melhoria da NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,24 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
 
     adiciona(event) {
         event.preventDefault();
-        alert('chamou o controller');
+        let helper = new DateHelper();
+
+        let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+            console.log(negociacao);
+            console.log(helper.dataParaTexto(negociacao.data));
+
+        
     }
 }
