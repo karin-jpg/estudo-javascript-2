d98605e9afe0b740913dda1f619d0633f70459f7
karin-jpg <andradkarin@gmail.com>
2021-03-10 21:52:23 -0300

    Criação da view para as negociacoes

---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,16 @@
+<table class="table table-hover table-bordered" id="tabela">
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+                <th>VOLUME</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+        </tbody>
+        
+        <tfoot>
+        </tfoot>
+    </table>
