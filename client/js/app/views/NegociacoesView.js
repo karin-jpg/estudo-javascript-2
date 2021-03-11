45c6c5a597801d4f20b3f329ed30c98a3c52d882
karin-jpg <andradkarin@gmail.com>
2021-03-10 22:41:12 -0300

    Atualização dinamica da tabela de negociações

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,16 +1,45 @@
-<table class="table table-hover table-bordered" id="tabela">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
+class NegociacoesView {
+
+    constructor(elemento){
+        this._elemento = elemento;
+    }
+
+    _template(modelo) {
+
+        return `
+            <table class="table table-hover table-bordered" id="tabela">
+                <thead>
+                   <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+             </thead>
         
-        <tbody>
-        </tbody>
+                <tbody>
+                    ${modelo.negociacoes.map(n => 
+                         `
+                            <tr>
+                                <td>${DateHelper.dataParaTexto(n.data)}</td>
+                                <td>${n.quantidade}</td>
+                                <td>${n.valor}</td>
+                                <td>${n.volume}</td>
+                            </tr>
+                        `
+                    ).join('')}
+                </tbody>
         
-        <tfoot>
-        </tfoot>
-    </table>
+                <tfoot>
+                <td colspan = "3"></td>
+                <td>${modelo.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td> 
+                </tfoot>
+            </table>
+            `;
+    }
+
+    update(modelo) {
+        this._elemento.innerHTML = this._template(modelo);
+    }
+
+}
